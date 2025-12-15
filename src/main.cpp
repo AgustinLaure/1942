3c5efa2f63c90614fd0ca6a534e0678a1f08ab14
Agustin <agustin.laure@estudiante.imagecampus.edu.ar>
2025-12-15 18:46:21 -0300

    init project

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include "raylib.h"
+
+int main()
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "1942");
+
+    while (!WindowShouldClose())   
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+
+    CloseWindow();        
+
+    return 0;
+}
---
+++ tools/copy-res.bat
@@ -0,0 +1,2 @@
+copy "%~1lib\raylib-5.5_win64_msvc16\lib\*.dll" "%~2"  /y 
+xcopy "%~1res\" "%~2res\" /E /I /Y
